f8795d8e04fb63e0a2df6fca90459f2cb3f83ac8
shaghayegh <bazrafkan.sh@gmail.com>
2025-07-01 19:36:08 +0330

    یکم تغیر برای تم روشن و..

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -21,16 +21,16 @@ function ContactUs() {
             پشتیبانی
           </h2>
         </div>
-        <h3 className="text-[130%] font-bold text-blue-300 px-1 pt-4">
+        <h3 className="text-[130%] font-bold text-blue-200 px-1 pt-4">
             پشتیبانی</h3>
             <p className=" p-2 pb-6">
             فقط از طریق پیام رسان های (تلگرام , واتس اپ , ایتا):
             09399619640</p>
-        <h3 className="text-[130%] font-bold text-blue-300 px-1 pt-4">
+        <h3 className="text-[130%] font-bold text-blue-200 px-1 pt-4">
             کانال ها</h3>
             <p className=" p-2 pb-6">
             کانال ما در ایتا و تلگرام : @shaghayegh</p>
-        <h3 className="text-[130%] font-bold text-blue-300 px-1 pt-4">
+        <h3 className="text-[130%] font-bold text-blue-200 px-1 pt-4">
             ادمین ثبت سفارش</h3>
             <p className=" p-2 pb-6">
             در پیام رسان تلگرام و ایتا : 09399619640 , @shaghayegh</p>
