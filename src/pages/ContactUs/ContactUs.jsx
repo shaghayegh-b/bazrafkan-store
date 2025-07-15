98c2340dc20a5665e3a9e4625500e463dee087a4
shaghayegh <bazrafkan.sh@gmail.com>
2025-07-15 17:30:16 +0330

    تغیراتی در پدینگ صفحه هایی که توسط فوتر ایجاد میشن

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -21,21 +21,21 @@ function ContactUs() {
         <h3 className="text-[130%] font-bold text-blue-200 px-1 pt-4">
           پشتیبانی
         </h3>
-        <p className=" p-2 pb-6">
+        <p className=" p-2 pb-6 px-3">
           فقط از طریق پیام رسان های (تلگرام , واتس اپ , ایتا):
           <a href="tel:09399619640">09399619640</a>
         </p>
         <h3 className="text-[130%] font-bold text-blue-200 px-1 pt-4">
           کانال ها
         </h3>
-        <p className=" p-2 pb-6">
+        <p className=" p-2 pb-6 px-3">
           کانال ما در ایتا و تلگرام : ,
           <a href="mailto:@bazrafkan">@bazrafkan</a>
         </p>
         <h3 className="text-[130%] font-bold text-blue-200 px-1 pt-4">
           ادمین ثبت سفارش
         </h3>
-        <p className=" p-2 pb-6">
+        <p className=" p-2 pb-6 px-3">
           در پیام رسان تلگرام و ایتا : <a href="tel:09399619640">09399619640</a>
           , <a href="mailto:@bazrafkan">@bazrafkan</a>
         </p>
