89fd5d472dad647cf2da5c759a48bca81633cbc6
shaghayegh <bazrafkan.sh@gmail.com>
2025-07-15 16:33:13 +0330

    درست کردن لینک های تماس و ایمیل جهت ارتباط سریع

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -10,7 +10,7 @@ function ContactUs() {
   }, []);
   return (
     <>
-<Header>ارتباط با ما</Header>
+      <Header>ارتباط با ما</Header>
       <div className="ContactUs px-2 py-1">
         <div className="relative w-[100%] mt-2">
           <img src={img} alt="" className="" />
@@ -22,17 +22,22 @@ function ContactUs() {
           پشتیبانی
         </h3>
         <p className=" p-2 pb-6">
-          فقط از طریق پیام رسان های (تلگرام , واتس اپ , ایتا): 09399619640
+          فقط از طریق پیام رسان های (تلگرام , واتس اپ , ایتا):
+          <a href="tel:09399619640">09399619640</a>
         </p>
         <h3 className="text-[130%] font-bold text-blue-200 px-1 pt-4">
           کانال ها
         </h3>
-        <p className=" p-2 pb-6">کانال ما در ایتا و تلگرام : @shaghayegh</p>
+        <p className=" p-2 pb-6">
+          کانال ما در ایتا و تلگرام : ,
+          <a href="mailto:@bazrafkan">@bazrafkan</a>
+        </p>
         <h3 className="text-[130%] font-bold text-blue-200 px-1 pt-4">
           ادمین ثبت سفارش
         </h3>
         <p className=" p-2 pb-6">
-          در پیام رسان تلگرام و ایتا : 09399619640 , @shaghayegh
+          در پیام رسان تلگرام و ایتا : <a href="tel:09399619640">09399619640</a>
+          , <a href="mailto:@bazrafkan">@bazrafkan</a>
         </p>
       </div>
       <Footer></Footer>
