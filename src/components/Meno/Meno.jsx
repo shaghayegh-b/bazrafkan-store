27e64df3c2374dd360eb9beea98fe5abd34c87f6
shaghayegh <bazrafkan.sh@gmail.com>
2025-07-24 11:15:50 +0330

    لینک دادن به همه محصولات حذف موارد اضافی بسته شدن منو با انتخاب هر گزینه از منو

--- src/components/Meno/Meno.jsx
+++ src/components/Meno/Meno.jsx
@@ -59,25 +59,28 @@ function Meno() {
           <div>
             <div className="Meno2 flex flex-col gap-2">
               <ul className="flex flex-col gap-3">
-                <li>
-                  <NavLink to="/bazrafkan-store/">
-                    <i className="fa fa-home text-[#1974ba]"></i>
-                    <span className="font-[600]">همه محصولات</span>
-                  </NavLink>
+                <li
+                  onClick={() => {
+                    setMeno(false);
+                    funcAxios(
+                      "https://686b9bdee559eba90873470f.mockapi.io/ap/bazrafkan-store/products"
+                    );
+                  }}
+                >
+                  <i className="fa fa-home text-[#1974ba]"></i>
+                  <span className="font-[600]">همه محصولات</span>
                 </li>
                 <li
                   className={`relative ${grouping ? "grouping" : ""}`}
                   onClick={() => setGrouping(!grouping)}
                 >
-                  <button>
-                    <i className="fas fa-store text-[#1974ba]"></i>
-                    <span className="font-[600]">دسته بندی</span>
-                    <i
-                      className={`fa fa-chevron-up absolute left-0 top-[7px] transition-all duration-300 ease-in-out ${
-                        grouping ? "rotate-[180deg]" : ""
-                      }`}
-                    ></i>
-                  </button>
+                  <i className="fas fa-store text-[#1974ba]"></i>
+                  <span className="font-[600]">دسته بندی</span>
+                  <i
+                    className={`fa fa-chevron-up absolute left-0 top-[7px] transition-all duration-300 ease-in-out ${
+                      grouping ? "rotate-[180deg]" : ""
+                    }`}
+                  ></i>
                   <div
                     className={`transition-all duration-1000 ease-in-out ${
                       grouping ? "" : "hidden"
@@ -89,7 +92,6 @@ function Meno() {
                       }`}
                     >
                       <li
-                        className="hover:   "
                         onClick={() => {
                           setMeno(false);
                           funcAxios(
@@ -100,7 +102,6 @@ function Meno() {
                         <p className=" tracking-tighter  p-1 pr-2">مانتو</p>
                       </li>
                       <li
-                        className="hover:   "
                         onClick={() => {
                           setMeno(false);
                           funcAxios(
@@ -113,7 +114,6 @@ function Meno() {
                         </p>
                       </li>
                       <li
-                        className="hover:   "
                         onClick={() => {
                           setMeno(false);
                           funcAxios(
@@ -124,7 +124,6 @@ function Meno() {
                         <p className=" tracking-tighter  p-1 pr-2">ست</p>
                       </li>
                       <li
-                        className="hover:   "
                         onClick={() => {
                           setMeno(false);
                           funcAxios(
@@ -137,7 +136,6 @@ function Meno() {
                         </p>
                       </li>
                       <li
-                        className="hover:   "
                         onClick={() => {
                           setMeno(false);
                           funcAxios(
@@ -150,7 +148,6 @@ function Meno() {
                         </p>
                       </li>
                       <li
-                        className="hover:   "
                         onClick={() => {
                           setMeno(false);
                           funcAxios(
@@ -163,7 +160,6 @@ function Meno() {
                         </p>
                       </li>
                       <li
-                        className="hover:   "
                         onClick={() => {
                           setMeno(false);
                           funcAxios(
@@ -176,7 +172,6 @@ function Meno() {
                         </p>
                       </li>
                       <li
-                        className="hover:   "
                         onClick={() => {
                           setMeno(false);
                           funcAxios(
@@ -189,25 +184,40 @@ function Meno() {
                     </ul>
                   </div>
                 </li>
-                <li>
+                <li
+                  onClick={() => {
+                    setMeno(false);
+                  }}
+                >
                   <Link to="/bazrafkan-store/FavProduct">
                     <i className="fa fa-star text-[#1974ba]"></i>
                     <span className="font-[600]">محصولات مورد علاقه</span>
                   </Link>
                 </li>
-                <li>
+                <li
+                  onClick={() => {
+                    setMeno(false);
+                  }}
+                >
                   <NavLink to="/bazrafkan-store/AskedQuestion">
                     <i className="fas fa-comments text-[#1974ba]"></i>
                     <span className="font-[600]">سوالات متداول</span>
                   </NavLink>
                 </li>
-                {/* <li>
+                {/* <li
+                                onClick={() => {
+                          setMeno(false);
+                        }}>
                   <a href="">
                     <i className="fa fa-bell text-[#1974ba]"></i>
                     <span className="font-[600]">اعلانات</span>
                   </a>
                 </li> */}
-                <li>
+                <li
+                  onClick={() => {
+                    setMeno(false);
+                  }}
+                >
                   <NavLink to="/bazrafkan-store/ContactUs">
                     <i className="fa fa-link text-[#1974ba]"></i>
                     <span className="font-[600]">ارتباط با ما</span>
@@ -222,7 +232,11 @@ function Meno() {
                   onClick={() => setIsDark(!isDark)}
                 >
                   <button type="submit" className="">
-                    <i className={`text-[#1974ba] fas ${isDark ? "fa-sun" : "fa-moon"}`}></i>
+                    <i
+                      className={`text-[#1974ba] fas ${
+                        isDark ? "fa-sun" : "fa-moon"
+                      }`}
+                    ></i>
                     <span className="font-[600]">
                       {isDark ? "حالت روز" : "حالت شب "}
                     </span>
@@ -233,6 +247,7 @@ function Meno() {
                   <li
                     onClick={() => {
                       logout(navigate);
+                      setMeno(false);
                     }}
                   >
                     <Link to="/bazrafkan-store/" className="font-[600]">
@@ -241,7 +256,11 @@ function Meno() {
                     </Link>
                   </li>
                 ) : (
-                  <li>
+                  <li
+                    onClick={() => {
+                      setMeno(false);
+                    }}
+                  >
                     <i className="fa fa-user-plus text-[#1974ba]"></i>
                     <Link to="/bazrafkan-store/Login">ساخت حساب کاربری</Link>
                   </li>
